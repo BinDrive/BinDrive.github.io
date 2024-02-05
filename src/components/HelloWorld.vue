0524adc904aac497394480ac83974dcaa4eec229
MAX_3 <max3@mail.com>
2024-02-05 05:29:49 +0300

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+import Camera from "simple-vue-camera";
+
+export default defineComponent({
+    components: {
+        Camera,
+    },
+});
+
+
+
+// <script setup>
+// import { defineComponent } from 'vue'
+// import Camera from 'simple-vue-camera'
+
+// export default defineComponent({
+//   components: {
+//     Camera
+//   }
+// })
+</script>
+<template>
+  <h1>cam1↓</h1>
+    <camera :resolution="{ width: 375, height: 812 }" autoplay></camera>
+</template>
