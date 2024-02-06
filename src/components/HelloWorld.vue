ca71756ef5a8db83a182ee7668402fa14564ce8f
Max3 <ma3@gmail.com>
2024-02-06 23:15:52 +0300

    6

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,26 +1,5 @@
-<script lang="ts">
-import { defineComponent } from "vue";
-import Camera from "simple-vue-camera";
-
-export default defineComponent({
-    components: {
-        Camera,
-    },
-});
-
-
-
-// <script setup>
-// import { defineComponent } from 'vue'
-// import Camera from 'simple-vue-camera'
-
-// export default defineComponent({
-//   components: {
-//     Camera
-//   }
-// })
-</script>
 <template>
-  <h1>cam1↓</h1>
-    <camera :resolution="{ width: 375, height: 812 }" autoplay></camera>
+  <div>
+    HELLO
+  </div>
 </template>
